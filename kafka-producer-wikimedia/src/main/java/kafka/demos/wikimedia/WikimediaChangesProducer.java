38e3c56f5fc7b8acbf78e82a414dae3448d9e01f
İsmail ŞENGÜL <isengul@havelsan.com.tr>
2024-11-29 09:35:21 +0300

    wikimedia producer operation module is added to git repository

---
+++ kafka-producer-wikimedia/src/main/java/kafka/demos/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,45 @@
+package kafka.demos.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "localhost:9092";
+
+        // Create Producer Properties
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(producerProperties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer,topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler,URI.create(url));
+        EventSource eventSource = builder.build();
+
+        //start the producer in another thread
+        eventSource.start();
+
+        //we produce for 10 min and block the program until then
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
