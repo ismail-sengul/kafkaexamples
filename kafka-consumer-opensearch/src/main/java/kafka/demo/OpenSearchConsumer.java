2a4e0ad05315e45c170a36fcfd5ac52367e6c8ae
İsmail ŞENGÜL <ismailsengultr61@gmail.com>
2024-12-04 17:52:18 +0300

    OpensearchConsumer project is finished and docker-compose.yml file is updated

--- kafka-consumer-opensearch/src/main/java/kafka/demo/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/kafka/demo/OpenSearchConsumer.java
@@ -1,27 +1,41 @@
 package kafka.demo;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
+import org.opensearch.action.index.IndexRequest;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
-    public static RestHighLevelClient createOpenSearchClient() {
+    private static RestHighLevelClient createOpenSearchClient() {
         //For your local connection String
-        String connectionString = "localhost:9200";
+        String connectionString = "http://localhost:9200";
 
         RestHighLevelClient restHighLevelClient;
         URI connectionUri = URI.create(connectionString);
@@ -29,10 +43,10 @@ public class OpenSearchConsumer {
         //extract login information if it exist
         String userInfo = connectionUri.getUserInfo();
 
-        if (userInfo != null) {
+        if (userInfo == null) {
             restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connectionUri.getHost(), connectionUri.getPort(), connectionUri.getScheme())));
-        }else {
-            String [] auth = userInfo.split(":");
+        } else {
+            String[] auth = userInfo.split(":");
 
             CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
             credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
@@ -46,6 +60,53 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String groupId = "consumer-opensearch-demo";
+
+        //Create Consumer Properties
+        Properties consumerProperties = new Properties();
+
+        consumerProperties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        consumerProperties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProperties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        return new KafkaConsumer<>(consumerProperties);
+
+    }
+
+    private static void addShutdownHook(KafkaConsumer<String, String> consumer) {
+        //get a referance to the main thread
+        final Thread mainThread = Thread.currentThread();
+
+        //adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run(){
+                logger.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
+
+                // join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+    private static String extractId(String json){
+        //gson lib
+        return JsonParser.parseString(json)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
+    }
+
     private static final Logger logger = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
 
     public static void main(String[] args) throws IOException {
@@ -53,26 +114,82 @@ public class OpenSearchConsumer {
         //First client the OpenSearch Client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
-        //Create the index of OpenSearch if it does not exist already
+        //Create our Kafka Client
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
 
-        try (openSearchClient){
+        //Add shutdown hook to main class thread
+        addShutdownHook(consumer);
+
+        //Create the index of OpenSearch if it does not exist already
+        try (openSearchClient; consumer) {
 
             boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
-            if(!indexExist){
+            if (!indexExist) {
                 CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
                 openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
                 logger.info("Wikimedia index has been created");
             }
+
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+
+            while (true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+
+                int recordCount = records.count();
+                logger.info("Received " + recordCount + " records");
+
+                BulkRequest bulkRequest = new BulkRequest();
+
+                for (ConsumerRecord<String, String> record : records) {
+                    // ID Strategies
+
+                    // Strategy 1
+                    // define an ID using Kafka Record Coordinates
+                    // String id = record.topic() + ":" + record.partition() + ":" + record.offset();
+
+                    // Strategy 2
+                    // extract the ID from the JSON value
+                    String id = extractId(record.value());
+
+                    //send the record into OpenSearch
+                    IndexRequest indexRequest = new IndexRequest("wikimedia")
+                            .source(record.value(), XContentType.JSON)
+                            .id(id);
+
+                    //For bulk request
+                    bulkRequest.add(indexRequest);
+
+                    //For single request
+
+                    //IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                    //logger.info(response.getId());
+                }
+
+                if (bulkRequest.numberOfActions() > 0){
+                    BulkResponse bulkResponse = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    logger.info("Inserted "+ bulkResponse.getItems().length + " records.");
+
+                    try {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                consumer.commitSync();
+                logger.info("Offsets have been committed");
+            }
+        } catch (WakeupException e){
+            logger.info("Consumer is starting to shut down.");
+        }catch (Exception e) {
+            logger.error("Unexpected exception in the consumer ", e);
+        }finally {
+            consumer.close(); //close the consumer, this will also commit offsets
+            openSearchClient.close();
+            logger.info("The consumer and OpenSearch Client are now gracefully shut down");
         }
-
-        //Create our Kafka Client
-
-        //main code logic
-
-        //close things
-        openSearchClient.close();
-
 
     }
 }
